d82bbf19e47628d93c62cd12bb8ef70f2a71c134
Rhey Louie Miaga <mrheylouie@gmail.com>
2026-03-03 19:50:17 +0800

    optimized frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: "/",
   build: {
+    chunkSizeWarningLimit: 1000,
+    reportCompressedSize: false, 
+    cssCodeSplit: true, 
+
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          animations: ['framer-motion'],
-          ui: ['@tremor/react', 'lucide-react'],
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'animations': ['framer-motion'],
+          'ui-components': ['@tremor/react', 'lucide-react'],
         },
       },
     },
   },
-})
+
+  server: {
+    port: 3000,
+    strictPort: true,
+  }
+});
